0dfd25b6759f5180ef849c3fc632bf220951d545
h098s02409 <h098s02409@chunchon.gwe.hs.kr>
2025-11-26 09:11:20 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -74,6 +74,10 @@ CONTENT_BY_LABEL: dict[str, dict[str, list[str]]] = {
     #   "images": ["https://.../jjampong1.jpg", "https://.../jjampong2.jpg"],
     #   "videos": ["https://youtu.be/XXXXXXXXXXX"]
     # },
+    labels[0] : {"texts" : ["중국식 냉면은 맛있어"], "images" : ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGeKHyvfkQyeKL-SjRdjPNjDxGfF7YlwK_TDKSR6s-WEWqUQQgpeS_aCuQ6K-wEA71vY4&usqp=CAU"]},
+    labels[1] : {"texts" : ["짜장면은 맛있어"], "images" : ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCIYe6tXLLLd58f6X8gr3gi3uSwsizc15GKQ&s"]},
+    labels[2] : {"texts" : ["짬뽕은 맛있어"], "images" : ["https://wimg.kyeongin.com/news/cms/2025/08/21/9/news-p.v1.20250718.8afaa460292242158f26b9c54599fc29_P4.webp"]},
+    labels[0] : {"texts" : ["탕수육은 맛있어"], "images" : ["https://blog.kakaocdn.net/dna/qnIz2/btqSdtiK7C4/AAAAAAAAAAAAAAAAAAAAAK_eDgChgNlbA3DwDL3Rhp3zyE68j5YgxkTq4Zz6RRN5/img.jpg?credential=yqXZFxpELC7KVnFOS48ylbz2pIh7yKj8&expires=1764514799&allow_ip=&allow_referer=&signature=IgQS1EsJodJN9xYMyJB6awE7Ae8%3D]},
 }
 
 # ======================
